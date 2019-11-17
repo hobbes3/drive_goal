19ff67648aead5c011917d742a16a8de3624cd68
Satoshi Kawasaki <hobbes3@gmail.com>
2019-11-17 11:10:13 +0000

    Big update using the new splunk_rest changes.

--- bin/lnd_get_drive_data.py
+++ bin/lnd_get_drive_data.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # hobbes3
 
-from splunk_rest.splunk_rest import *
+import logging
+import json
+import splunk_rest.splunk_rest as sr
+from splunk_rest.splunk_rest import rest_wrapped
 
 def send_and_get_drive_data(arg):
     drive_url = arg["drive_url"]
@@ -9,7 +12,7 @@ def send_and_get_drive_data(arg):
     skip = arg["skip"]
 
     params = {
-        "take": DRIVE_TAKE,
+        "take": take,
         "skip": skip
     }
 
@@ -18,62 +21,29 @@ def send_and_get_drive_data(arg):
     if r and r.text:
         r_json = r.json()
         rows = r_json.get("data", [])
-        log("Current position.", level="debug", extra={"skip": skip, "row_count": len(rows)}, stdout=False)
+        logger.debug("Current position.", extra={"skip": skip, "row_count": len(rows)})
 
         data = ""
         for row in rows:
             event = {
-                "index": DRIVE_INDEX,
+                "__session_id": sr.session_id,
+                "index": sr.config["drive"]["index"],
                 "sourcetype": "drive_" + drive_type + "_json",
-                "source": str(script_file),
+                "source": __file__,
                 "event": row,
             }
 
             data += json.dumps(event)
 
-        log("Sending data to Splunk...", stdout=False)
-        s.post(HTTP_URL, headers=HTTP_HEADERS, data=data)
+        logger.debug("Sending data to Splunk...")
+        s.post(sr.config["hec"]["url"], headers=sr.config["hec"]["headers"], data=data)
 
 @rest_wrapped
 def drive_rest_api():
-## WORKDAY EMPLOYEES
-#    log("Workday: Getting employee list...")
-#    workday_auth = HTTPBasicAuth(
-#        WORKDAY_USER,
-#        WORKDAY_PASS
-#    )
-#    workday_url = "https://services1.myworkday.com/ccx/service/customreport2/splunk/tetang/SPLK_L_D_Headcount"
-#    workday_params = {
-#        "format": "json"
-#    }
-#
-#    r = s.get(workday_url, params=workday_params, auth=workday_auth)
-#
-#    if r:
-#        r_json = r.json()
-#        employees = r_json["Report_Entry"]
-#        employee_count = len(employees)
-#
-#        log("Found employees.".format(employee_count))
-#
-#        data = ""
-#        for employee in employees:
-#            event = {
-#                "index": WORKDAY_INDEX,
-#                "sourcetype": "workday_employee_json",
-#                "source": script_file,
-#                "event": employee,
-#            }
-#
-#            data += json.dumps(event)
-#
-#        logger.info("Workday: Sending data to Splunk...")
-#        s.post(HTTP_URL, headers=HTTP_HEADERS, data=data)
-
 # DRIVE GOALS
     arg_list = []
 
-    log("Getting total goals count...")
+    logger.info("Getting total goals count...")
     drive_url = "https://api.highground.com/1.0/Goals"
     drive_params = {
         "take": 1
@@ -84,16 +54,16 @@ def drive_rest_api():
     if r and r.text:
         r_json = r.json()
         total = r_json.get("total", 0)
-        log("Found goals.", level="debug", extra={"goal_total": total})
+        logger.info("Found goals.", extra={"goal_total": total})
 
         arg_list.extend([{
             "drive_url": drive_url,
             "drive_type": "goal",
             "skip": i,
-        } for i in range(0, total, DRIVE_TAKE)])
+        } for i in range(0, total, take)])
 
 # DRIVE CHECK-INS
-    log("Getting total check-ins count...")
+    logger.info("Getting total check-ins count...")
     drive_url = "https://api.highground.com/1.0/CheckInSessions"
     drive_params = {
         "take": 1
@@ -105,16 +75,16 @@ def drive_rest_api():
         r_json = r.json()
         total = r_json.get("total", 0)
 
-        log("Found check-ins.", level="debug", extra={"checkin_total": total})
+        logger.info("Found check-ins.", extra={"checkin_total": total})
 
         arg_list.extend([{
             "drive_url": drive_url,
             "drive_type": "checkin",
             "skip": i,
-        } for i in range(0, total, DRIVE_TAKE)])
+        } for i in range(0, total, take)])
 
 # DRIVE 360 FEEDBACKS
-    log("Getting total feedbacks count...")
+    logger.info("Getting total feedbacks count...")
     drive_url = "https://api.highground.com/1.0/FeedbackSessions"
     drive_params = {
         "take": 1
@@ -125,27 +95,26 @@ def drive_rest_api():
     if r and r.text:
         r_json = r.json()
         total = r_json.get("total", 0)
-        log("Found feedbacks.", level="debug", extra={"feedback_total": total})
+        logger.info("Found feedbacks.", extra={"feedback_total": total})
 
         arg_list.extend([{
             "drive_url": drive_url,
             "drive_type": "feedback",
             "skip": i,
-        } for i in range(0, total, DRIVE_TAKE)])
+        } for i in range(0, total, take)])
 
-        if DEBUG:
+        if debug:
             arg_list = arg_list[:10]
 
-    log("Getting and sending drive data...")
-    for _ in tqdm(pool.imap_unordered(send_and_get_drive_data, arg_list), total=len(arg_list), disable=args.silent):
-        pass
+    logger.info("Getting and sending drive data...")
+    sr.multiprocess(send_and_get_drive_data, arg_list)
 
 if __name__ == "__main__":
-    s = retry_session()
+    logger = logging.getLogger("splunk_rest.splunk_rest")
+    s = sr.retry_session()
 
-    drive_headers = {
-        "clientkey": DRIVE_API_KEY,
-        "Accept": "application/json",
-    }
+    debug = sr.config["general"]["debug"]
+    drive_headers = sr.config["drive"]["headers"]
+    take = sr.config["drive"]["take"]
 
     drive_rest_api()
